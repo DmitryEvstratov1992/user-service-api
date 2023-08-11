d238b6e6bcde220d12df48f4a8a8ac75ec1141fe
Evstratov Dmitry <www.shepard1992@gmail.com>
2023-08-11 07:22:32 +0300

    Update .gitignore

---
+++ src/test/java/com/citizen/userserviceapi/UserServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citizen.userserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
